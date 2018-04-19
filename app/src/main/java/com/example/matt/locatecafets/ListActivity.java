625e51398a5629dc09b1a5fd00b813cd94c60ce3
mattInnov <mat.alingrin@gmail.com>
2018-04-19 19:42:22 +0300

    progress on list activity

--- app/src/main/java/com/example/matt/locatecafets/ListActivity.java
+++ app/src/main/java/com/example/matt/locatecafets/ListActivity.java
@@ -1,13 +1,167 @@
 package com.example.matt.locatecafets;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class ListActivity extends Activity {
+
+    private int maxDistance = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) { //Checks the permission to use Location
+            if (getIntent().getParcelableArrayListExtra("cafets") != null) {
+                // Getting Current Location
+                LocationManager locationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 600, 50, locationListener);
+                Location myLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                ArrayList<Cafeteria> cafetArrList = getIntent().getParcelableArrayListExtra("cafets");
+                if (myLocation != null) {
+                    LatLng myCoordinates = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
+                    updateOrderList(myLocation, cafetArrList);
+                    ViewGroup resultContainer = findViewById(R.id.result_container);
+                    LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                    int childCount = resultContainer.getChildCount();
+                    if (childCount != 0) {
+                        for (int i = 0; i < childCount; i++) {
+                            resultContainer.removeViewAt(i);
+                        }
+                    }
+                    for (int i = 0; i < cafetList.length; i++) {
+                        if (cafetList[i].getDistanceToMe() < maxDistance) {
+                            String text = String.valueOf(i + 1) + ". " + cafetList[i].getName() + " is at " + String.valueOf(cafetList[i].getDistanceToMe()) + "m";
+                            View result = inflater.inflate(R.layout.result_item, null);
+                            TextView resultText = (TextView) ((ViewGroup) result).getChildAt(0);
+                            resultText.setText(text);
+                            resultContainer.addView(result);
+                            final int indexCafet = i;
+                            ((ViewGroup) result).getChildAt(2).setOnClickListener(
+                                    new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View v) {
+                                            Cafeteria cafet = cafetList[indexCafet];
+                                            Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                                            intent.putExtra("cafet", cafet);
+                                            startActivity(intent);
+                                        }
+                                    }
+                            );
+                        }
+                    }
+                } else {
+                    Toast.makeText(this, "Position not found", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+
+        };
+
+    /*
+
+            if (ActivityCompat.checkSelfPermission(
+                    getApplicationContext(),
+                    Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(),
+                    Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            LocationManager locationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 50, locationListener);
+            Location myLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (myLocation != null) {
+                updateOrderList(myLocation);
+                ViewGroup resultContainer = findViewById(R.id.result_container);
+                LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                int childCount = resultContainer.getChildCount();
+                if ( childCount != 0) {
+                    for (int i = 0; i < childCount; i++) {
+                        resultContainer.removeViewAt(i);
+                    }
+                }
+                for (int i=0; i< cafetList.length; i++){
+                    if (cafetList[i].getDistanceToMe() < maxDistance) {
+                        String text =  String.valueOf(i+1) + ". " + cafetList[i].getName() + " is at " + String.valueOf(cafetList[i].getDistanceToMe()) + "m";
+                        View result = inflater.inflate(R.layout.result_item, null);
+                        TextView resultText = (TextView) ((ViewGroup)result).getChildAt(0);
+                        resultText.setText(text);
+                        resultContainer.addView(result);
+                        final int indexCafet = i;
+                        ((ViewGroup)result).getChildAt(2).setOnClickListener(
+                                new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        Cafeteria cafet = cafetList[indexCafet];
+                                        Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                                        intent.putExtra("cafet", cafet);
+                                        startActivity(intent);
+                                    }
+                                }
+                        );
+                    }
+                }
+            } else  {
+                Toast.makeText(MainActivity.this, "Position not found", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    });
+}*/
+
+    public void updateOrderList(Location myLocation, ArrayList<Cafeteria> cafetArrList) {
+        for (Cafeteria cafet : cafetArrList) {
+            float dist = cafet.getLocation().distanceTo(myLocation);
+            cafet.setDistanceToMe((int)dist);
+        }
+        Collections.sort(cafetArrList, new Comparator<Cafeteria>() {
+            @Override
+            public int compare(Cafeteria c1, Cafeteria c2) {
+                return (c1.getDistanceToMe() - c2.getDistanceToMe());
+            }
+        });
     }
+
+    LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location loc) {
+            updateOrderList(loc);
+        }
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {}
+        @Override
+        public void onProviderEnabled(String provider) {}
+        @Override
+        public void onProviderDisabled(String provider) {}
+    };
 }
--- app/src/main/java/com/example/matt/locatecafets/MainActivity.java
+++ app/src/main/java/com/example/matt/locatecafets/MainActivity.java
@@ -44,28 +44,28 @@ public class MainActivity extends Activity {
 
     //Initialising data
     private Cafeteria aura = new Cafeteria("Aura",
-            62.6036007,29.7420141,
+            62.6036007, 29.7420141,
             "Yliopistokatu 2 , 80100 Joensuu");
     private Cafeteria carelia = new Cafeteria("Carelia",
-            62.6048478,29.7422755,
+            62.6048478, 29.7422755,
             "Yliopistokatu 4, 80100 Joensuu");
     private Cafeteria futura = new Cafeteria("Futura, Natura and Metria",
-            62.6036007,29.73201292,
+            62.6036007, 29.73201292,
             "Yliopistokatu 7 , 80100 Joensuu");
     private Cafeteria pipetti = new Cafeteria("Pipetti",
-            62.6036007,29.730141,
+            62.6036007, 29.730141,
             "Yliopistokatu 2 , 80100 Joensuu");
     private Cafeteria kuutti = new Cafeteria("Kuutti",
-            62.6036007,26.7420141,
+            62.6036007, 26.7420141,
             "Yliopistokatu 2 , 80100 Joensuu");
     private Cafeteria pihlaja = new Cafeteria("Pihlaja",
-            62.6036007,20.7420141,
+            62.6036007, 20.7420141,
             "Yliopistokatu 2 , 80100 Joensuu");
     private Cafeteria metla = new Cafeteria("Metla",
-            62.6136007,29.8270141,
+            62.6136007, 29.8270141,
             "Yliopistokatu 2 , 80100 Joensuu");
     private Cafeteria verola = new Cafeteria("Verola",
-            62.6036007,22.7420141,
+            62.6036007, 22.7420141,
             "Yliopistokatu 2 , 80100 Joensuu");
 
     private Cafeteria[] cafetList = {aura, carelia, futura, pipetti, kuutti, pihlaja, metla, verola};
@@ -92,89 +92,15 @@ public class MainActivity extends Activity {
         });
 
 
-
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (ActivityCompat.checkSelfPermission(
-                        getApplicationContext(),
-                        Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(),
-                        Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-                    return;
-                }
-                LocationManager locationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
-                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 50, locationListener);
-                Location myLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                if (myLocation != null) {
-                    updateOrderList(myLocation);
-                    ViewGroup resultContainer = findViewById(R.id.result_container);
-                    LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                    int childCount = resultContainer.getChildCount();
-                    if ( childCount != 0) {
-                        for (int i = 0; i < childCount; i++) {
-                            resultContainer.removeViewAt(i);
-                        }
-                    }
-                    for (int i=0; i< cafetList.length; i++){
-                        if (cafetList[i].getDistanceToMe() < maxDistance) {
-                            String text =  String.valueOf(i+1) + ". " + cafetList[i].getName() + " is at " + String.valueOf(cafetList[i].getDistanceToMe()) + "m";
-                            View result = inflater.inflate(R.layout.result_item, null);
-                            TextView resultText = (TextView) ((ViewGroup)result).getChildAt(0);
-                            resultText.setText(text);
-                            resultContainer.addView(result);
-                            final int indexCafet = i;
-                            ((ViewGroup)result).getChildAt(2).setOnClickListener(
-                                new View.OnClickListener() {
-                                    @Override
-                                    public void onClick(View v) {
-                                        Cafeteria cafet = cafetList[indexCafet];
-                                        Intent intent = new Intent(MainActivity.this, MapsActivity.class);
-                                        intent.putExtra("cafet", cafet);
-                                        startActivity(intent);
-                                    }
-                                }
-                            );
-                        }
-                    }
-                } else  {
-                    Toast.makeText(MainActivity.this, "Position not found", Toast.LENGTH_SHORT).show();
-                }
-
+                Intent intent = new Intent(MainActivity.this, ListActivity.class);
+                ArrayList<Cafeteria> cafetArrList = new ArrayList<>(Arrays.asList(cafetList));
+                intent.putParcelableArrayListExtra("cafets", cafetArrList);
+                startActivity(intent);
             }
         });
     }
-
-    public void updateOrderList(Location myLocation) {
-        for (Cafeteria cafet : cafetList) {
-            float dist = cafet.getLocation().distanceTo(myLocation);
-            cafet.setDistanceToMe((int)dist);
-        }
-        Arrays.sort(cafetList, new Comparator<Cafeteria>() {
-            @Override
-            public int compare(Cafeteria c1, Cafeteria c2) {
-                return (c1.getDistanceToMe() - c2.getDistanceToMe());
-            }
-        });
-    }
-
-    LocationListener locationListener = new LocationListener() {
-        @Override
-        public void onLocationChanged(Location loc) {
-            updateOrderList(loc);
-        }
-        @Override
-        public void onStatusChanged(String provider, int status, Bundle extras) {}
-        @Override
-        public void onProviderEnabled(String provider) {}
-        @Override
-        public void onProviderDisabled(String provider) {}
-    };
 
 }
