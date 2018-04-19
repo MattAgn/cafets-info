17262fc566c427a3edd269c6edf49d43fa4efd2b
mattInnov <mat.alingrin@gmail.com>
2018-04-19 11:18:37 +0300

    home screen started

--- app/src/main/java/com/example/matt/locatecafets/MainActivity.java
+++ app/src/main/java/com/example/matt/locatecafets/MainActivity.java
@@ -13,6 +13,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -74,6 +75,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
         Button searchButton = findViewById(R.id.search_button);
