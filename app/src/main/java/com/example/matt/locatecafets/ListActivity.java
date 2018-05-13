da3793087f73de361aa5002ff1ac6ad63a90262d
mattInnov <mat.alingrin@gmail.com>
2018-05-13 10:15:30 +0300

    Added a check on permissions

--- app/src/main/java/com/example/matt/locatecafets/ListActivity.java
+++ app/src/main/java/com/example/matt/locatecafets/ListActivity.java
@@ -14,18 +14,15 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.content.ContextCompat;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.PendingResult;
--- app/src/main/java/com/example/matt/locatecafets/MainActivity.java
+++ app/src/main/java/com/example/matt/locatecafets/MainActivity.java
@@ -9,8 +9,10 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -48,6 +50,8 @@ import static com.example.matt.locatecafets.Database.getCafeterias;
 -          have a link to the menu list from the cafeteria webpage */
 
 public class MainActivity extends AppCompatActivity {
+
+    final private int GRANTED_LOCATION = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -94,12 +98,51 @@ public class MainActivity extends AppCompatActivity {
                 finalOpeningHours += newLine;
             }
             cafet.setOpeningHours(finalOpeningHours);
-
             //Price unknown
             if (cafet.getPrice() == -1) {
                 cafet.setPriceString(getString(R.string.not_available));
             }
         }
 
+        // Asking for permission to use GPS if not granted before
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        GRANTED_LOCATION);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case GRANTED_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Thanks, you can now use the app !", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "You should have enable the GPS ;p", Toast.LENGTH_LONG).show();
+                    Intent homeIntent = new Intent(Intent.ACTION_MAIN);
+                    homeIntent.addCategory( Intent.CATEGORY_HOME );
+                    homeIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(homeIntent);
+                }
+            }
+
+        }
     }
 }
